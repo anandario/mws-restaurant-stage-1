82fc2a78c62d60558ef46cbeff76ecfc8b25ce4c
Ananda Rio <anandarionunez@gmail.com>
2019-01-21 09:15:48 +0100

    CSS improvements

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -168,6 +168,7 @@ class DBHelper {
     const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng], {
       title: restaurant.name,
       alt: restaurant.name,
+      transparent: true,
       url: DBHelper.urlForRestaurant(restaurant)
     })
     marker.addTo(newMap);
